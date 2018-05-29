def5d6269eab17b3a1c1c20efd1a067514a1fe70
Andrew Polk <polk@umail.ucsb.edu>
2018-05-29 02:30:08 -0700

    Create App.java

---
+++ app/src/main/java/com/example/victoriasneddon/chipchat/App.java
@@ -0,0 +1,12 @@
+package com.example.victoriasneddon.chipchat;
+
+import android.app.Application;
+
+//Simple Application extension to cache the Bitmap images while the app exits
+public class App extends Application {
+
+    //Public Variables -----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------
+    ChatRoom chatroom;
+    String roomName;
+}
